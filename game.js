1f9128ccb5938d56c6792879fa5a5f8078078ff7
Arjinoodles <arjinoodles@gmail.com>
2025-02-08 10:01:03 -0800

    Abstracted Deck

--- game.js
+++ game.js
@@ -17,42 +17,55 @@ class Card{
         }
     }
 } 
+class Deck{
+    fullDeck= new Array(52);
+    constructor(){
+       
+        var tempSuite=""
+        var index=0;
+        for(var i=0; i<4; i++){
+            for(var j=1; j<14;j++){
+                if(i==0){
+                 this.fullDeck[index]= new Card("Spades",j)
+                }
+                if(i==1){
+                    this.fullDeck[index]= new Card("Hearts",j)
+                }
+                if(i==2){
+                    this.fullDeck[index]= new Card("Diamonds",j)
+                }
+                if(i==3){
+                    this.fullDeck[index]= new Card("Clubs",j)
+                }
+                index++
+                   
+            }
+        }
+    } //constructor ends 
 
+    shuffle(){
+        for (let i = this.fullDeck.length - 1; i > 0; i--){
+     
+            // Pick a random index from 0 to i inclusive
+            let j = Math.floor(Math.random() * (i + 1)); 
+            // Swap fullDeck[i] with the element at the random index
+            [this.fullDeck[i], this.fullDeck[j]] = [this.fullDeck[j], this.fullDeck[i]];
+        } 
+    }
 
-var fullDeck= new Array(52);
-var tempSuite=""
-var index=0;
-for(var i=0; i<4; i++){
-    for(var j=1; j<14;j++){
-        if(i==0){
-         fullDeck[index]= new Card("Spades",j)
-        }
-        if(i==1){
-            fullDeck[index]= new Card("Hearts",j)
-        }
-        if(i==2){
-            fullDeck[index]= new Card("Diamonds",j)
-        }
-        if(i==3){
-            fullDeck[index]= new Card("Clubs",j)
-        }
-        index++
-           
+    print(){
+        //print the full deck
+        this.fullDeck.forEach((element) => console.log(element));
     }
 }
 
-function randomize (arr) {
- 
-    
-    for (let i = arr.length - 1; i > 0; i--){
-     
-        // Pick a random index from 0 to i inclusive
-        let j = Math.floor(Math.random() * (i + 1)); 
-        // Swap arr[i] with the element at the random index
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    } 
-} 
 
-//print the entire deck
-randomize(fullDeck)
-fullDeck.forEach((element) => console.log(element));
+
+
+
+var deck= new Deck();
+console.log("Unshuffled deck: ")
+deck.print();
+deck.shuffle();
+console.log("Shuffled deck")
+deck.print();
